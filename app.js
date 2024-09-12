84ea3288984302cf11d4f0fdfeefe02ade37aa94
Argas-commits <118813434+Argas-commits@users.noreply.github.com>
2024-09-12 20:27:52 +0400

    trying to reanable subasecreate client

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Replace these with your actual Supabase URL and API Key from the Supabase dashboard
 const SUPABASE_URL = 'https://sleghazbpzgynnzriozz.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZWdoYXpicHpneW5uenJpb3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4ODc5MDQsImV4cCI6MjA0MTQ2MzkwNH0.ltjHJAEnQBYko4Om6pwQRU5xp6QsQfkYyZwEBKG71xA';
-/*
+
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-*/
+
 
 
 let patCount = 281;
